c93992fcb7a498a49dbf7ce50bcb9ac01257bbff
skyprince999 <aakashg80@gmail.com>
2024-01-26 14:06:50 +0530

    Update globe.js

--- js/globe.js
+++ js/globe.js
@@ -4,7 +4,7 @@ let world;
 
 const getVal = (feat) =>
   feat.properties.GDP_MD_EST / Math.max(1e5, feat.properties.POP_EST);
-fetch("../data/countries_small.geojson") //_updated_Jan2024
+fetch("../data/countries_small_updated_Jan2024.geojson") //
   .then((res) => res.json())
   .then((countries) => {
     const maxVal = Math.max(...countries.features.map(getVal));
